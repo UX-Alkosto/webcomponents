91af5d270439b1debe9113a94dec0403f409f053
Miguel Colmenares <me@miguelcolmenares.com>
2021-07-19 22:12:29 -0500

    🚚 Renamed JS files
    Renamed JS files to avoid the aditional index.js file

--- components/acordeon/index.js
+++ components/acordeon/index.js
@@ -1,1 +1,36 @@
-export {Acordeon} from'./acordeon';
+import { LitElement, html } from 'lit';
+import { styles } from './css';
+export class Acordeon extends LitElement {
+    constructor(){
+        super();
+        this.open=false;
+        this.title='Escriba un título';
+    }
+    static get properties() {
+        return {
+            open: {type:String,reflect:true},
+            title: {type:String}
+        };
+    }
+    static get styles(){
+        return styles;
+    }
+    firstUpdated() {
+        this.open = isTrueSet(this.getAttribute('open'));
+    }
+    render(){
+        return html`<details ?open="${this.open}">
+        <summary>
+            <h3 class="tit_pregunta">${this.title}</h3>
+            <k-icon class="expand" icon="alk-icon-abajo"></k-icon>
+            <k-icon class="collapse" icon="alk-icon-cerrar1"></k-icon>
+        </summary>
+        <hr class="linea">
+        <slot></slot>
+    </details>`;
+    }
+}
+function isTrueSet(value = '') {
+    if(typeof value !== 'string') return false;
+    return value.toLowerCase() === 'true' && true;
+}
