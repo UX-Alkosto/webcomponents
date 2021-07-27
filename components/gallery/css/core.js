a812fe422c79ba1dc0916690c9f85cedced765d5
Miguel Colmenares <me@miguelcolmenares.com>
2021-07-26 21:59:41 -0500

    ♻️ Refactor k-gallery component to extend slider class

--- components/gallery/css/core.js
+++ components/gallery/css/core.js
@@ -1,75 +1,27 @@
 import { css } from 'lit';
 export const Core = css`
-.k-gallery {
-    box-sizing: border-box;
-    position: relative;
-    width: 100%;
-}
-.k-gallery * {
-    box-sizing: inherit;
-}
-.k-gallery__track {
-    overflow: hidden;
-}
-.k-gallery__slides {
-    align-items: stretch;
-    backface-visibility: hidden;
-    background-color: var(--k-gallery-background-color, #fff);
-    display: flex;
-    flex-wrap: nowrap;
-    list-style: none;
-    overflow: hidden;
-    padding: .5em 0;
-    position: relative;
-    touch-action: pan-Y;
-    transform-style: preserve-3d;
-    width: 100%;
-    will-change: transform;
-    white-space: nowrap;
-}
-.k-gallery__slides--dragging {
-    user-select: none;
-}
+.k-slider__slide,
 .k-gallery__slide {
+    -webkit-tap-highlight-color: transparent;
+    -webkit-touch-callout: none;
     align-items: stretch;
     display: flex;
-    justify-content: center;
-    width: 100%;
-    height: auto;
-    flex-shrink: 0;
-    white-space: normal;
-    user-select: none;
-    -webkit-touch-callout: none;
-    -webkit-tap-highlight-color: transparent;
     filter: grayscale(100%);
+    flex-shrink: 0;
+    height: auto;
+    justify-content: center;
     opacity:0.6;
+    user-select: none;
+    white-space: normal;
+    width: 100%;
 }
-
-
-
-.k-gallery__slide img{
-    max-width: 100%;
+.k-slider__slide img,
+.k-gallery__slide img {
     max-width: 100%;
     object-fit: cover;
 }
+.k-slider__slide k-card,
 .k-gallery__slide k-card {
     max-height: 100%;
     width: 100%;
-}
-.k-gallery__slide a {
-    user-select: none;
-    -webkit-user-drag: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-}
-.k-gallery__arrows {
-    -webkit-touch-callout: none;
-    user-select: none;
-}
-.k-gallery__bullets {
-    -webkit-touch-callout: none;
-    user-select: none;
-}
-.k-gallery--rtl {
-    direction: rtl;
 }`;
--- components/gallery/index.js
+++ components/gallery/index.js
@@ -1,145 +1,15 @@
-import { LitElement, html } from 'lit';
-import { common } from '../common';
-import { styles, Core as coreStyles, Theme as themeStyles } from './css';
+import { Slider } from '../slider';
+import { Core as coreStyles, Theme as themeStyles } from './css';
 
-export class Gallery extends LitElement {
-    static get properties() {
-        return {
-            'arrows': { type: Boolean },
-            'bullets': { type: Boolean },
-            'desktop-arrows': { type: Boolean },
-            'desktop-bullets': { type: Boolean },
-            'desktop-items': { type: Number },
-            'items-space': { type: Number },
-            loaded: { type: Boolean, reflect: true },
-            'mobile-arrows': { type: Boolean },
-            'mobile-bullets': { type: Boolean },
-            'mobile-items': { type: Number },
-            peek: { type: Number },
-            width: { type: Number }
-        };
-    }
+export class Gallery extends Slider {
     static get styles() {
         return [
-            styles,
+            super.styles,
             coreStyles,
             themeStyles
         ];
     }
     constructor() {
         super();
-        this.arrows = false;
-        this.bullets = false;
-        this.countSlides = this.childElementCount;
-        this['desktop-arrows'] = false;
-        this['desktop-bullets'] = false;
-        this['desktop-items'] = 6;
-        this['items-space'] = 16;
-        this.loaded = false;
-        this['mobile-arrows'] = false;
-        this['mobile-bullets'] = false;
-        this['mobile-items'] = 2;
-        this.peek = 0;
-        this.width = common.defaultMaxWidth;
     }
-    attributeChangedCallback(name, oldval, newval) {
-        super.attributeChangedCallback(name, oldval, newval);
-        this.dispatchEvent(new Event(`${name}-changed`));
-        // update columns
-        if (name === 'desktop-items') {
-            this.style.gridTemplateColumns = `repeat(${this.columns}, 1fr)`;
-        }
-        // update rows
-        else if (name === 'mobile-items') {
-            this.style.gridTemplateRows = `repeat(${this.rows}, 1fr)`;
-        }
-    }
-    firstUpdated() {
-        this.arrows = isTrueSet(this.getAttribute('arrows'));
-        this['desktop-arrows'] = isTrueSet(this.getAttribute('desktop-arrows')) ?
-            true : this.arrows;
-        this['desktop-bullets'] = isTrueSet(this.getAttribute('desktop-bullets')) ?
-            true : this.bullets;
-        this['mobile-arrows'] = isTrueSet(this.getAttribute('mobile-arrows')) ?
-            true : this.arrows;
-        this['mobile-bullets'] = isTrueSet(this.getAttribute('mobile-bullets')) ?
-            true : this.bullets;
-        this._initSlider();
-    }
-    render() {
-        return html`${this._dynamicStyles()}
-        <div class="k-gallery">
-            <div data-glide-el="track" class="k-gallery__track">
-                <slot class="k-gallery__slides"></slot>
-            </div>
-            ${(this['desktop-arrows'] || this['mobile-arrows']) ? this._getArrows() : ''}
-            ${(this['desktop-bullets'] || this['mobile-bullets']) ? this._getDots() : ''}
-        </div>`;
-    }
-    _dynamicStyles() {
-        const width = `${(this.width === 0 || this.width === null || this.width === undefined) ? 1204 : this.width}`;
-        return html`<style>
-            :host {
-                max-width: ${width}px;
-            }
-        </style>`;
-    }
-    _getArrows() {
-        return html`<div class="k-gallery__arrows" data-glide-el="controls">
-            <button class="k-gallery__arrow k-gallery__arrow--left" data-glide-dir="<"><k-icon icon="alk-icon-izquierda"></k-icon></button>
-            <button class="k-gallery__arrow k-gallery__arrow--right" data-glide-dir=">"><k-icon icon="alk-icon-derecha"></k-icon></button>
-        </div>`;
-    }
-    _getDots() {
-        const bullets = [];
-        for (let index = 0; index < this.countSlides; index++) {
-            bullets.push(html`<button class="k-gallery__bullet" data-glide-dir="=${index}"></button>`);
-        }
-        return html`<div class="k-gallery__bullets" data-glide-el="controls[nav]">
-            ${bullets}
-        </div>`;
-    }
-    async _initSlider() {
-        const Glide = await import('@glidejs/glide').then(Module => Module.default);
-        let rawSlides = this.innerHTML;
-        const slides = this.shadowRoot.querySelector('.k-gallery__slides');
-        slides.innerHTML = rawSlides;
-        this.innerHTML = '';
-        this.loaded = true;
-        return new Glide(this.shadowRoot.querySelector('.k-gallery'), {
-            classes: {
-                direction: {
-                    ltr: 'k-gallery--ltr',
-                    rtl: 'k-gallery--rtl'
-                },
-                slider: 'k-gallery--slider',
-                carousel: 'k-gallery--carousel',
-                swipeable: 'k-gallery--swipeable',
-                dragging: 'k-gallery--dragging',
-                cloneSlide: 'k-gallery__slide--clone',
-                activeNav: 'k-gallery__bullet--active',
-                activeSlide: 'k-gallery__slide--active',
-                disabledArrow: 'k-gallery__arrow--disabled'
-            },
-            gap: this['items-space'],
-            type: 'carousel',
-            peek: this.peek,
-            perView: this['desktop-items'],
-            breakpoints: {
-                768: {
-                    perView: this['mobile-items'],
-                    peek: 0
-                },
-                359: {
-                    perView: 1,
-                    peek: 0
-                }
-            }
-        }).mount();
-    }
-}
-
-function isTrueSet(value = '') {
-    if(typeof value !== 'string') return false;
-    return value.toLowerCase() === 'true' && true;
 }
