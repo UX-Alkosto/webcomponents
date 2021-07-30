b1b08bc9515aabcc468415e465308f0c36f59f14
Miguel Colmenares <me@miguelcolmenares.com>
2021-07-29 22:37:46 -0500

    Updated NPM dependecies
    Updated Rollup config
    Added recommended VS Code extensions
    Added prettier config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
+import { babel } from '@rollup/plugin-babel';
 import alias from '@rollup/plugin-alias';
 import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import cleaner from 'rollup-plugin-cleaner';
-import filesize from 'rollup-plugin-filesize';
 import { terser } from 'rollup-plugin-terser';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import cleanup from 'rollup-plugin-cleanup';
+import summary from 'rollup-plugin-summary';
 import { name, version } from './package.json';
 
-// The main JavaScript bundle for modern browsers that support
-// JavaScript modules and other ES2015+ features.
-const config = {
+export default {
     input: {
         components: 'index.js'
     },
@@ -17,15 +17,34 @@ const config = {
         banner: `/*! ${name} release: ${version} */`,
         dir: 'dist',
         chunkFileNames: '[name]-[hash].js',
-        format: 'es'
+        format: 'esm'
     }],
+    onwarn(warning) {
+        if (warning.code !== 'THIS_IS_UNDEFINED') {
+            console.error(`(!) ${warning.message}`);
+        }
+    },
     plugins: [
         cleaner({
             targets: [
                 './dist/'
             ]
         }),
+        replace({
+            'Reflect.decorate': 'undefined',
+            preventAssignment: true
+        }),
         minifyHTML(),
+        terser({
+            ecma: 2020,
+            module: true,
+            warnings: true,
+            mangle: {
+                properties: {
+                    regex: /^__/,
+                },
+            }
+        }),
         resolve(),
         alias({
             entries: [{
@@ -36,19 +55,10 @@ const config = {
         cleanup({
             comments: 'none'
         }),
-        filesize({
-            showMinifiedSize: true,
-            showGzippedSize: true
-        })
+        babel({
+            babelHelpers: 'bundled'
+        }),
+        summary()
     ],
-    preserveEntrySignatures: false
+    preserveEntrySignatures: 'strict'
 };
-
-if (process.env.NODE_ENV !== 'development') {
-    config.plugins.push(terser({
-        module: true,
-        warnings: true
-    }));
-}
-
-export default config;
