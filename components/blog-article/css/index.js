e1f508d3be40e2018bf0b963ecc044f3d42e5c26
Miguel Colmenares <me@miguelcolmenares.com>
2021-08-17 16:40:48 -0500

    Rollback Changes

--- components/blog-article/css/index.js
+++ components/blog-article/css/index.js
@@ -3,10 +3,10 @@ import { common } from '../../common';
 
 const styles = css`:host{
     background-color: var(--k-card-background-color, #fff);
-    border-radius: var(--button-radius,28px);
+    border-radius: var(--k-card-border-radius, 4px);
     display: inline-flex;
     flex-direction: column;
-    margin: 2em 0.5em 0.5em;
+    margin: .5em;
     max-width: 100%;
     padding: 0;
     position:relative;
@@ -24,25 +24,18 @@ const styles = css`:host{
     box-shadow: 0 2px 5px 2px rgba(0,0,0,.2);
 }
 ::slotted(k-button){
-    border: 3px solid rgb(255, 255, 255);
-    border-radius: var(--button-radius,28px);
-    font-size: 14px;
-    font-weight: bold;
-    line-height: 21px;
-    min-height: inherit;
-    min-width: 170px;
-    padding: 0.75em !important;
+    border: 4px solid white;
+    min-width: 180px;
     position: absolute;
-    right: 1em;
-    top: 125px;   
+    right: 2em;
+    top: 164px;
     z-index: 1;
 }
 ::slotted(img){
-    border-radius: var(--button-radius,28px) var(--button-radius,28px) 0 0;
-    height: 150px !important;
-    margin-bottom: 10px;
-    object-fit: cover;
-    width: 100%;   
+    border-radius: var(--button-radius,4px) var(--button-radius,4px) 0 0;
+    height: 191px;
+    object-fit:cover;
+    width:100%;
 }
 .text{
     padding: .5em 1.75em 1.75em 1.75em;
@@ -50,17 +43,16 @@ const styles = css`:host{
 
 ::slotted(span){
     color: var(--primary-color1,#F9C332);
-    font-size: var(--font-base, 12px);
+    font-size: var(--font-base, ${unsafeCSS(common.fontSize.p)});
     font-weight: bold;
     line-height: 1.572em;
 }
 ::slotted(h2){
     color: var(--text-color1,#010000);
-    font-size: 20px !important;
+    font-size: ${unsafeCSS(common.fontSize.h2)};
     font-weight: bold;
     line-height: 1.3em;
-    margin: 15px 0px !important;
-    max-width: 380px;   
+    max-width: 380px;
 }
 
 ::slotted(p){
@@ -69,16 +61,15 @@ const styles = css`:host{
     display: -webkit-box;
     -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
-    font-size: var(--font-base, 13px);
-    line-height: 18px;
-    margin-bottom: 10px !important;
+    font-size: var(--font-base, ${unsafeCSS(common.fontSize.p)});
+    line-height: 1.572em;
 }
 ::slotted(.author_date){
     color: var(--text-color3, #959595);
-    font-size: 12px;
+    font-size: 0.857em;
     font-weight: medium;
     line-height: 1.572em;
-    margin-bottom: 0 !important;
+    margin-bottom: 0;
     text-align: right;
 }
 
